ea4201c67eeda1263850ae17a65481ee4fc4d5df
Shariar Islam <shariarn85@gmail.com>
2024-03-23 06:47:06 +0600

    Type Assertion

---
+++ src/type-assertion.ts
@@ -0,0 +1,31 @@
+let amni: any;
+
+emni = "Next level web development";
+
+(emni as string).length; //both are same
+<string>emni.length; //both are same. But this format won't work in tsx format
+
+const kgToGram = (param: string | Number): string | number | undefined => {
+  if (typeof param === "string") {
+    const value = parseFloat(param) * 1000;
+    return `The COnverted result is: ${value} gram`;
+  }
+  if (typeof param === "number") {
+    const value = param * 1000;
+    return value;
+  }
+};
+
+const resultToBeNumber = kgToGram(5) as number;
+const resultToBeNumber1 = <string>kgToGram(5);
+
+const resultToBeString = kgToGram("7") as string;
+const resultToBeString1 = <number>kgToGram("10");
+
+type CustomErrorType = {
+  message: string;
+};
+try {
+} catch (error) {
+  console.log((error as CustomErrorType).message);
+}
