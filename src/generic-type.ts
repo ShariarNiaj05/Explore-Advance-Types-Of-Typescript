97724910bd6fd928d16ae4dffea09e4f72f67884
Shariar Islam <shariarn85@gmail.com>
2024-03-25 06:38:23 +0600

    Introduction Of Generic In Type

--- src/generic-type.ts
+++ src/generic-type.ts
@@ -1,2 +1,29 @@
-const genericTypeRollNumbers: Array<number> = [44, 12, 4];
+type GenericType<T> = Array<T>;
+
+/* const genericTypeRollNumbers: Array<number> = [44, 12, 4];
 const genericTypeRollNumbers2: Array<string> = ["44", "12", "4"];
+
+const userNameAndRollNumber: Array<{ name: string; roll: number }> = [
+  { name: "Shariar", roll: 25 },
+]; */
+
+const genericTypeRollNumbers: GenericType<number> = [44, 12, 4];
+const genericTypeRollNumbers2: GenericType<string> = ["44", "12", "4"];
+
+const userNameAndRollNumber: GenericType<{ name: string; roll: number }> = [
+  { name: "Shariar", roll: 25 },
+];
+
+type GenericTuple<X, Y> = [X, Y];
+const relation: GenericTuple<string, string> = ["Shariar", "Kate Winslet"];
+
+const relationWithSalary: GenericTuple<
+  { name: string; salary: number },
+  string
+> = [
+  {
+    name: "Niaj",
+    salary: 10000000,
+  },
+  "Kate Winslet",
+];
